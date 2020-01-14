b5c80153d92d68d915e57492ccfba80a2037bd97
Vicente Oyanedel <vicenteoyanedel@gmail.com>
2020-01-14 18:27:50 -0300

    Serial processing for MFCC.

--- features.py
+++ features.py
@@ -523,6 +523,11 @@ class MelCepstralCoefficientsFeatureExtractor(FeatureExtractor):
 
         return __process_element
 
+    def transform(self, parallel=False, **kwargs):
+        if parallel:
+            raise Exception('error: {} cannot be ran in paralel'.format(self.feature_name))
+        return super().transform(parallel, **kwargs)
+
 
 # MFSC
 class WindowedMelSpectralCoefficientsFeatureExtractor(FeatureExtractor):
