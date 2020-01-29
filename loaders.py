0b6e26d0412d1e1fdbe6e615e0eab06e938a5016
Vicente Oyanedel <vicenteoyanedel@gmail.com>
2020-01-29 04:26:41 -0300

    Fixed MFCC random crop parameter to config gmm_max_frames

--- loaders.py
+++ loaders.py
@@ -12,7 +12,7 @@ from torch.utils.data.sampler import Sampler
 from torchvision import transforms
 
 from config import FEATURES_DATA_PATH, RESNET_MIN_DIM, ADISAN_BATCH_SIZE, ADISAN_EPOCHS, WAVEFORM_MAX_SEQUENCE_LENGTH, \
-    WAVEFORM_NUM_CHANNELS, WAVEFORM_SAMPLE_RATE
+    WAVEFORM_NUM_CHANNELS, WAVEFORM_SAMPLE_RATE, GMM_FRAME_LIMIT
 
 
 # def get_shuffle_split(self, n_splits=2, test_size=0.5, train_size=0.5):
@@ -781,7 +781,7 @@ class CepstrumDataset(ExperimentDataset):
             label_encoder
         )
         self.transform = transforms.Compose([
-            CepstrumDataset.RandomCropMFCC(1000 * 5),  # ]  # with numpy
+            CepstrumDataset.RandomCropMFCC(GMM_FRAME_LIMIT),  # ]  # with numpy
             #self.ToTensor(),
         ]  # with torch
         )
