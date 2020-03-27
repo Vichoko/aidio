f95c28d6253d23858fa5fa3ecd0d7e9ba4a7f0ab
Vicente Oyanedel <vicenteoyanedel@gmail.com>
2020-03-26 22:00:35 -0300

    Fixed positional encoding

--- torch_models.py
+++ torch_models.py
@@ -536,13 +536,8 @@ class WaveNetTransformerClassifier(nn.Module):
             kernel_size=WAVENET_POOLING_KERNEL_SIZE,
             stride=WAVENET_POOLING_STRIDE
         )
-
         max_seq_len = math.floor(
-            (max_raw_sequnece - (WAVENET_POOLING_KERNEL_SIZE - 1)) / (WAVENET_POOLING_STRIDE) + 1)
-        print('info: max_raw_sequnece {}'.format(max_raw_sequnece))
-        print('info: WAVENET_POOLING_KERNEL_SIZE {}'.format(WAVENET_POOLING_KERNEL_SIZE))
-        print('info: WAVENET_POOLING_KERNEL_SIZE {}'.format(WAVENET_POOLING_KERNEL_SIZE))
-        print('info: max_seq_len {}'.format(max_seq_len))
+            (max_raw_sequnece - (WAVENET_POOLING_KERNEL_SIZE - 1)) / WAVENET_POOLING_STRIDE + 1)
         self.positional_encoder = PositionalEncoder(
             d_model,
             max_seq_len=max_seq_len
