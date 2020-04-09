4a50daf87581cba9130f5e1c4b1d686131b9ba1b
Vicente Oyanedel <vicenteoyanedel@gmail.com>
2020-04-09 16:50:10 -0400

    Fix split_songs.py

--- commands/split_songs.py
+++ commands/split_songs.py
@@ -47,6 +47,8 @@ def make_handler(new_labels, new_filenames, data_path, out_path):
                 new_filename = filename.replace(extension, 'mfcc.{}.npy'.format(slice_idx))
                 if os.path.isfile(out_path / new_filename):
                     print('{} already exists skipping...'.format(new_filename))
+                    new_filenames.append(new_filename)
+                    new_labels.append(label)
                     continue
                 # Normalize audio signal
                 wav = librosa.util.normalize(wav)
@@ -88,7 +90,6 @@ def main():
     df = pd.read_csv(data_path / label_filename)
     filenames = df['filename']
     labels = df['label']
-
     new_filenames = []
     new_labels = []
     # class_set = set()
@@ -102,7 +103,7 @@ def main():
     else:
         with concurrent.futures.ThreadPoolExecutor(max_workers=FEATURE_EXTRACTOR_NUM_WORKERS) as executor:
             iterator = executor.map(handler, filenames, labels)
-        list(iterator) # wait to finish
+        list(iterator)  # wait to finish
     df = pd.DataFrame(np.asarray([new_filenames, new_labels]).swapaxes(0, 1))
     df.columns = ['filename', 'label']
     df.to_csv(out_path / label_filename, index=False)
