4882770a182be979399c3a9941545df406c8887b
Vichoko <vicenteoyanedel@gmail.com>
2020-11-13 21:34:39 -0300

    ckpt

---
+++ commands/ckpt_fixer.py
@@ -0,0 +1,30 @@
+"""
+Remove metric records on state_dict.
+
+"""
+
+import argparse
+from pathlib import Path
+
+import torch
+
+state_dict_keys_to_remove = ['test_acc.total', 'test_acc.correct',
+                             'val_acc.total', 'val_acc.correct',
+                             'train_acc.total', 'val_acc.correct', ]
+
+
+def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--src_path', help='', )
+    parser.add_argument('--dest_path', help='', )
+    args = parser.parse_args()
+    src_path = Path(args.src_path)
+    dest_path = Path(args.dest_path)
+    ckpt = torch.load(src_path)
+    for k in state_dict_keys_to_remove:
+        del ckpt['state_dict'][k]
+    torch.save(ckpt, dest_path)
+
+
+if __name__ == '__main__':
+    main()
